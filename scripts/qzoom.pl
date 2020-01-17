9b385ac63504e3e9e41126de9102c8ea634c74fc
Andrea Telatin <andrea@telatin.com>
2020-01-17 09:17:48 +0000

    Added rename

--- scripts/qzoom.pl
+++ scripts/qzoom.pl
@@ -12,20 +12,20 @@ if (-d "$RealBin/lib") {
 	use lib "$RealBin/lib";
 }
 use YAML::Tiny;
-say $YAML::Tiny::VERSION;
 our $AUTHOR  = 'Andrea Telatin';
 our $VERSION = '1.10';
 our $this_program = basename($0);
 our $biom_found = 0;
 my ($opt_cite,
-	$opt_help, 
-	$opt_version, 
+	$opt_help,
+	$opt_version,
 	$opt_debug,
 	$opt_data,
 	$opt_citation_file,
 	$opt_extract,
 	$opt_info,
 	$opt_verbose,
+	$opt_rename,
 );
 my $opt_outdir =  "./";
 my $result = GetOptions(
@@ -36,6 +36,7 @@ my $result = GetOptions(
 
  	'x|extract'   => \$opt_extract,
  	'o|outdir=s'  => \$opt_outdir,
+	'r|rename'    => \$opt_rename,
 
 	'debug'       => \$opt_debug,
 	'v|version'   => \$opt_version,
@@ -52,19 +53,21 @@ foreach my $opt_filename (@ARGV) {
 
 	debug(" - Loading <$opt_filename>");
 
+	# Check input
 	if ( ! -f "$opt_filename" ) {
-		say STDERR " - Skipping \"$opt_filename\": not found" if ($opt_verbose);
+		verbose(" - Skipping \"$opt_filename\": not found");
 		next;
 	}
 	if ($opt_filename !~/qz[av]$/) {
-		say STDERR " - Skipping \"$opt_filename\": not a .qza or .qzv file"if ($opt_verbose);
+		verbose(" - Skipping \"$opt_filename\": not a .qza or .qzv file");
 		next;
 	}
-	 
+
 	our $basename = basename($opt_filename);
 	our $artifact = getArtifact($opt_filename);
 	our $output;
 
+	# >>> CITATIONS
 	if (defined $opt_cite) {
 		say " - getting citation " if ($opt_debug);
 		my $citation = getArtifactText($artifact->{id}.'/provenance/citations.bib', $opt_filename);
@@ -78,8 +81,9 @@ foreach my $opt_filename (@ARGV) {
 			say "$citation";
 		}
 	}
-
+	# >>> DATA
 	if (defined $opt_data) {
+		say STDERR RED " [WARNING] This function is under development", RESET;
 		say BOLD $opt_filename, RESET;
 		my $c=0;
 		for my $file (@{ $artifact->{data} }) {
@@ -88,6 +92,7 @@ foreach my $opt_filename (@ARGV) {
 		}
 	}
 
+	# >>> INFO (DEFAULT)
 	if (defined $opt_info) {
 		my $key = color('red'). '[HTML Visualization]';
 		my $list = '';
@@ -103,13 +108,14 @@ foreach my $opt_filename (@ARGV) {
 			}
 		}
 		say GREEN, $artifact->{id}, RESET, "\t", $opt_filename, "\t", BOLD, $key ,RESET;
-		 
+
 		say Dumper $artifact if ($opt_debug);
-	} 
+	}
 	if (defined $opt_extract) {
 		debug("Extracting artifact to $opt_outdir");
 
 		if (0) {
+			# TODO: Remove unused conditional
 			say STDERR " - Specify output dir (-o) to extract the following files:";
 			foreach my $i ( @{$artifact->{data} } ) {
 				say  $i;
@@ -117,9 +123,10 @@ foreach my $opt_filename (@ARGV) {
 		} else {
 			if (! -d "$opt_outdir") {
 				run( qq(mkdir "$opt_outdir"), { 'description' => "Creating output directory <$opt_outdir>" } );
-			} 
+			}
 
 			if (! $artifact->{type} eq 'Visualization') {
+				# Extract VISUALIZATION
 				verbose("Extracting $opt_filename data");
 					run(
 						qq(unzip -j  -o "$opt_filename" '$artifact->{id}/data/*' -d "$opt_outdir"),
@@ -140,9 +147,9 @@ foreach my $opt_filename (@ARGV) {
 							my $out = $base;
 							$out =~s/biom/tsv/;
 							my $BiomConvert = qq(biom convert --to-tsv -i "$opt_outdir/$base" -o "$opt_outdir/$out");
-							
-							
-							run($BiomConvert, 
+
+
+							run($BiomConvert,
 							{
 									'description' => "Converting BIOM to TSV ($base)",
 									'error'       => "Unable to convert $opt_outdir/$base to TSV using 'biom' tool",
@@ -150,36 +157,71 @@ foreach my $opt_filename (@ARGV) {
 						}
 					}
 			} else {
+				# Extract ARTIFACT (qza)
 				my $destination_dir = $basename;
 				$destination_dir =~s/\./_/;
+				if ($opt_rename) {
+						if ( scalar @{ $artifact->{data}} != 1 ) {
+							die "This artifact contains more than one file: try removing -r\n";
+						} else {
+							verbose("Extracting $opt_filename visualization");
+							my ($dest_base) = $basename=~/^(.+)\.qz.$/;
+							my $source_file = basename(@{ $artifact->{data}}[0]);
+							my ($source_ext)  = $source_file=~/\.([^\.]+)$/;
+							my $dest_file     = $dest_base . '.' . $source_ext;
 
-				verbose("Extracting $opt_filename visualization");
-				run(
-					qq(unzip  -o "$opt_filename" '$artifact->{id}/data/*' -d "$opt_outdir"),
-						{
-							'description' => "Extracting 'data' from $opt_filename to $opt_outdir",
-							'error'       => "Unable to extract data."
+							debug("Will move $source_file to $dest_file");
+							run(
+								qq(unzip  -o "$opt_filename" '$artifact->{id}/data/*' -d "$opt_outdir"),
+									{
+										'description' => "Extracting 'data' from $opt_filename to $opt_outdir",
+										'error'       => "Unable to extract data."
+									}
+							);
+							run(
+								qq(mv "$opt_outdir"/$artifact->{id}/data/$source_file "$opt_outdir/$dest_file"),
+									{
+										'description' => "Moving 'data' from $opt_filename to $opt_outdir",
+										'error'       => "Unable to move data."
+									}
+							);
+							run(
+								qq(rmdir "$opt_outdir"/$artifact->{id}/data "$opt_outdir"/$artifact->{id}/),
+									{
+										'description' => "Cleanup",
+										'error'       => "Unable to remove a supposedly empty directory."
+									}
+							);
 						}
-				);
-				run(
-					qq(mv "$opt_outdir"/$artifact->{id}/data/ "$opt_outdir/$destination_dir/"),
-						{
-							'description' => "Moving 'data' from $opt_filename to $opt_outdir",
-							'error'       => "Unable to move data."
-						}
-				);
-				run(
-					qq(rmdir "$opt_outdir"/$artifact->{id}/),
-						{
-							'description' => "Cleanup",
-							'error'       => "Unable to remove a supposedly empty directory."
-						}
-				);
+				} else {
+						verbose("Extracting $opt_filename artifact");
+						run(
+							qq(unzip  -o "$opt_filename" '$artifact->{id}/data/*' -d "$opt_outdir"),
+								{
+									'description' => "Extracting 'data' from $opt_filename to $opt_outdir",
+									'error'       => "Unable to extract data."
+								}
+						);
+						run(
+							qq(mv "$opt_outdir"/$artifact->{id}/data/ "$opt_outdir/$destination_dir/"),
+								{
+									'description' => "Moving 'data' from $opt_filename to $opt_outdir",
+									'error'       => "Unable to move data."
+								}
+						);
+						run(
+							qq(rmdir "$opt_outdir"/$artifact->{id}/),
+								{
+									'description' => "Cleanup",
+									'error'       => "Unable to remove a supposedly empty directory."
+								}
+						);
+				}
 			}
-			
+
 
 		}
-		
+
 	}
 
 
@@ -210,7 +252,7 @@ sub version {
     print "$this_program $VERSION ($AUTHOR)\n";
      exit 0;
 }
- 
+
 sub usage {
     # Short usage string in case of errors
     print "To view info:
@@ -235,7 +277,7 @@ sub getArtifactText {
 	my ($file, $filename) = @_;
 
 	die "getArtifactText(x, y): y missing\n" unless (defined $filename);
-	
+
 	my $cmd_opt;
 	my $cmd = qq(unzip -p "$filename" "$file");
 
@@ -266,22 +308,22 @@ sub getArtifact {
 			my $stripped_path = $root;
 			$stripped_path.= '/' . join('/', @path) if ($path[0]);
 			$files{$stripped_path} = $1;
-			
+
 			if (! defined $artifact_id) {
 				$artifact_id = $id;
 			} elsif ($artifact_id ne $id) {
 				die "ARTIFACT PARSING ERROR:\nArtifact has multiple roots ($artifact_id but also $id).\n";
 			}
 			if ($root eq 'data') {
-				
+
 				push(@data, $stripped_path);
-				
+
 
 			} elsif ($root eq 'provenance') {
 				if ($path[0] eq 'artifacts') {
 					$parents{$path[1]}++;
 				}
-				
+
 			}
 
 		}
@@ -308,8 +350,8 @@ sub run {
 	debug("cmd:". $command);
 
 	if ($opt->{'description'}) {
-		debug("(". $opt->{"description"} .')') 
-	}  
+		debug("(". $opt->{"description"} .')')
+	}
 	if ($? and ! $opt->{no_die}) {
 		print STDERR "ERROR RUNNING EXTERNAL COMAND:\n";
 		print STDERR "Command: '$command' (",$opt->{'description'},")\n";
@@ -333,43 +375,43 @@ sub checkBin {
     run($commandString, $opt_command);
 }
 __END__
- 
+
 =head2 NAME
- 
+
 B<qzoom.pl> - a helper utility to extract data from Qiime2 artifact
- 
+
 =head2 AUTHOR
- 
+
 Andrea Telatin <andrea@telatin.com>
- 
+
 =head2 SYNOPSIS
- 
+
 qzoom.pl [options] <artifact1.qza/v> [<artifact2.qza ...]
- 
+
 =head2 OPTIONS
- 
+
 
 =head3 Main Actions
 
 
 =over 2
 
-B<-i, --info> 
+B<-i, --info>
 
-Print artifact citation to STDOUT or to file, is a filepath is provided. 
+Print artifact citation to STDOUT or to file, is a filepath is provided.
 Enabled by default if no C<--cite> or C<--extract> are defined.
 
-B<-c, --cite> 
+B<-c, --cite>
 
 Print artifact citation to STDOUT or to file. Specify -b FILE to save it.
 
-B<-x, --extract> 
+B<-x, --extract>
 
-Print the list of files in the 'data' directory. 
+Print the list of files in the 'data' directory.
 If a OUTDIR is provided, extract the content of the 'data' directory (i.e. the actual output of the artifact).
 Will create the directory if not found. Will overwrite files in the directory.
 
-B<-d, --data> 
+B<-d, --data>
 
 List all the files contained in the ./data directory of the artifacts
 
@@ -390,32 +432,37 @@ B<-b, --bibtex> I<FILE>
 
 Save citations to a file (append), to use with C<-c>, C<--cite>.
 
+B<-r, --rename>
 
-B<--verbose>  
+Rename the content of the artifact to {artifactbasename}.{ext}.
+Used with C<-x> will extract C<dna-sequences.fasta> from C<dada2repseq.qza> as
+C<dada2repseq.fasta>. Only works with single file artifacts.
+
+B<--verbose>
 
 Print verbose output.
 
 =back
- 
+
 =head2 BUGS
- 
+
 Please report them to <andrea@telatin.com>
- 
+
 =head2 COPYRIGHT
- 
-Copyright (C) 2019 Andrea Telatin 
- 
+
+Copyright (C) 2019 Andrea Telatin
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
- 
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
- 
+
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
- 
+
 =cut
