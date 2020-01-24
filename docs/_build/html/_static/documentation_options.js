be85fc0bf29c02a3b4c1356380b076776ee53c33
Andrea Telatin <andrea@telatin.com>
2020-01-24 11:27:42 +0100

    change theme

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'alpha',
+    VERSION: '0.10.3-alpha',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
